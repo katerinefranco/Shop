04e850b86083cfdd98907ed52ab0f45b79609f3c
KATERINE FRANCO <kfp@atp.net.co>
2019-06-10 15:53:56 -0500

    Products controller

---
+++ Shop.Web/Data/Entities/DataContext.cs
@@ -0,0 +1,20 @@
+﻿namespace Shop.Web.Data.Entities
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+
+
+
+    public class DataContext : DbContext //usa el entiti framework para .net core
+    {
+
+        public DbSet<Product> Products { get; set; } //se va a mapear como una tabla, se va a crear la clase como una bd
+
+
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)//conexion a la BD en el contructor
+        {
+
+        }
+    }
+}
---
+++ Shop.Web/Data/Entities/Product.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Shop.Web.Data.Entities
+{
+
+    using System;
+using System.ComponentModel.DataAnnotations;
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C2)", ApplyFormatInEditMode = false)] //moneda de 2decimales = false
+        public decimal Price { get; set; }
+
+        [Display(Name ="Image")] //display es para que lo muestre solo como image no con ImageUrl
+        public string ImageUrl { get; set; }
+
+        [Display(Name = "Last Purchase")]
+        public DateTime LastPurchase { get; set; }
+
+        [Display(Name = "Last Sale")]
+        public DateTime LastSale { get; set; }
+
+        [Display(Name = "Is availabe?")]
+        public bool IsAvailable { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N2)", ApplyFormatInEditMode = false)] //Numerico de 2 decimales = false
+        public double Stock { get; set; }
+
+    }
+}
